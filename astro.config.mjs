ca0740dd67937b8c3e3dd4a17329bb29dbbe3ea9
KouWeb <kou.development.web@gmail.com>
2026-04-28 10:41:16 +0900

    メニュー修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,17 +17,17 @@ export default defineConfig({
 				},
 			},
 			sidebar: [
-				{
-					label: "Guides",
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: "Example Guide", slug: "guides/example" },
-					],
-				},
-				{
-					label: "プロジェクト管理",
-					autogenerate: { directory: "guides" },
-				},
+				// {
+				// 	label: "Guides",
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: "Example Guide", slug: "guides/example" },
+				// 	],
+				// },
+				// {
+				// 	label: "プロジェクト管理",
+				// 	autogenerate: { directory: "guides" },
+				// },
 				{
 					label: "Visual Studio Code",
 					autogenerate: { directory: "vscode" },
